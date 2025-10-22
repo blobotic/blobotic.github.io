cbf9b237cf3df093abbe1470f898a895b8b3f18f
blobotic <vli3@andrew.cmu.edu>
2025-10-21 21:02:38 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+// the just fail and the unjust triumph; that’s why complacency is the most unforgivable of sins
+
+var typewriter = new Typewriter('#typewriter', {
+    loop: false,
+    delay: 53,
+    cursor: ""
+  });
+  
+typewriter
+    .typeString("but how could you live and have no story to tell? - white nights")
+    .start();
+
+
+  
